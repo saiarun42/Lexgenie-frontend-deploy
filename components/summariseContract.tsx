80e74c9e21fbfc5681c8984b1ddb9835b3e70be8
arun v <arun.v@iicl.in>
2025-10-10 11:11:31 +0530

    i commented out generate uuid

--- components/summariseContract.tsx
+++ components/summariseContract.tsx
@@ -8,13 +8,13 @@ import  {v4 as uuidv4} from 'uuid';
 
 
 // UUID Generator function for cross-browser compatibility
-function generateUUID() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        const r = Math.random() * 16 | 0;
-        const v = c === 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
-    });
-}
+//function generateUUID() {
+ //   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+ //       const r = Math.random() * 16 | 0;
+ //       const v = c === 'x' ? r : (r & 0x3 | 0x8);
+ //       return v.toString(16);
+ //   });
+//}
 
 type ChatMessage = {
     text: string;
